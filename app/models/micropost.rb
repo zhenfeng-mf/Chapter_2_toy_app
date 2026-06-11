c85a62d37e64d2c0b9fefb3cc58e095bf54031af
zhenfeng-mf <liao.zhenfeng@moneyforward.co.jp>
2026-06-11 17:41:45 +0900

    Fixed CI errors

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
 class Micropost < ApplicationRecord
-  validates :content, length: {maximum: 140}, presence: true
+  validates :content, length: { maximum: 140 }, presence: true
   belongs_to :user
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -17,7 +17,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create micropost" do
     assert_difference("Micropost.count") do
-      post microposts_url, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+      post microposts_url, params: { micropost: { content: "@micropost.content", user_id: @micropost.user_id } }
     end
 
     assert_redirected_to micropost_url(Micropost.last)
